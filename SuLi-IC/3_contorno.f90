ccc312d4b67e5c11ebf0544a22c954139eb38ce4
Pedro Miguel De Andrade Filho <pmiguel975@outlook.com>
2022-06-15 16:57:03 -0300

    Att 3_contorno

--- SuLi-IC/3_contorno.f90
+++ SuLi-IC/3_contorno.f90
@@ -63,6 +63,7 @@ if ((nlock == 1).or.(nlock == 2)) then
 		 v(:,1,:) = v(:,ny1,:)   + dpdy(:,1,:)
 		 w(:,0,:) = w(:,ny,:)    + dpdz(:,0,:)
 		!Free-slip condition
+		!No-slip condition
 		endif
 
 		if (ccy0.eq.1) then ! free-slip condition
